1a7a3fa2407adf25dc8c6a69d4530653782e30d1
Teguh Firmansyah <firmansyahtgh13@gmail.com>
2023-01-15 19:36:11 +0800

    menyederhanakan code

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const app = express()
-const home = require('./routes/home');
 const about = require('./routes/about');
 const contact = require('./routes/contact');
 
-app.use('/', home);
-app.use('/home', home);
 app.use('/about', about);
 app.use('/contact', contact);
+
+app.use('/', (req, res) => {
+    res.send('<h1>Home Page</h1>')
+})
 
 app.listen(3000, () => {
   console.log('Server running on port 3000')
--- routes/home.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-
-router.get('/', (req, res) => {
-    res.send('<h1>Home Page</h1>')
-  });
-  
-  module.exports = router;
